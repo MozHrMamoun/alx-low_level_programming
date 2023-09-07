29b1f8917364a53cbe25d84dd12a4801ad06a698
Muzhir <mamoun.muzhir@gmail.com>
2023-09-07 04:23:11 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,14 @@
  * main - Entry point
  *
  * Description: print a array using write function
- * 	write(int fd,const void *buf,size_t count);
- * 	
- * Return 1 (Failed)
+ *	write(int fd,const void *buf,size_t count);
+ *
+ * Return: 1 (error).
 */
 
 int main(void)
-{
+{	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, arr, 59);
-	return(1);
+	return (1);
 }

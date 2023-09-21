58a49f2378466e16050bc3199ddaa07d9571d1bb
Muzhir <mamoun.muzhir@gmail.com>
2023-09-21 09:52:21 -0700

    test

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,16 +8,14 @@ char *leet(char *str)
 {
 	char *pStr = str;
 	char letters[] = {'A', 'E', 'O', 'T', 'L'};
-	int val[] = {0, 1, 3, 4, 7};
+	int val[] = {4, 3, 0, 7, 1};
 	int i;
 
 	while (*str)
 	{
 		for (i = 0; i < sizeof(letters) / sizeof(val); i++)
-		{
 			if (*str == letters[i] || *str == letters[i] + 32)
 				*str = 48 + val[i];
-		}
 		str++;
 	}
 	return (pStr);

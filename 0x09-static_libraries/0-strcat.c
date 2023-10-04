731c5a1cc4b74bdcfdc4931eaf6800064cbc04ca
Muzhir <mamoun.muzhir@gmail.com>
2023-10-04 10:10:58 -0700

    lib

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c1, c2;
+
+	c1 = 0;
+	while (dest[c1])
+		c1++;
+
+	for (c2 = 0; src[c2] ; c2++)
+		dest[c1++] = src[c2];
+
+	return (dest);
+}
